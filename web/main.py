16fd27fdef503c3e0f22e7631af76b32dfe5184c
Diogo Lima <diogooo@gmail.com>
2024-09-27 12:05:27 -0300

    oauth

--- web/main.py
+++ web/main.py
@@ -1,15 +1,11 @@
 import sentry_sdk
-from fastapi import FastAPI
-from config.settings import SENTRY_DSN
-from .controllers.auth import (
-    register,
-    login_user,
-    activate_user as activate_user_controller,
-)
-from .request.auth import UserCreate, Login, UserActivate
-from .response.auth import User, LoginResponse
-
-from .middleware.authorization import BasicAuthBackend, CustomAuthMiddleware
+from fastapi import Depends, FastAPI, Request
+from fastapi_oauth2.middleware import OAuth2Middleware, Auth, User
+from fastapi_oauth2.config import OAuth2Config, OAuth2Client
+from fastapi_oauth2.router import router as oauth2_router
+from fastapi_oauth2.security import OAuth2
+from social_core.backends import open_id_connect
+from config.settings import SENTRY_DSN, JWT_SECRET, JWT_ALGORITHM, JWT_EXPIRES, OAUTH_CLIENT_ID, OAUTH_CLIENT_SECRET
 
 
 sentry_sdk.init(
@@ -21,21 +17,35 @@ sentry_sdk.init(
 )
 
 
+async def on_auth(auth: Auth, user: User):
+    ...
+
+oauth2 = OAuth2()
+
+class OIDConnect(open_id_connect.OpenIdConnectAuth):
+    OIDC_ENDPOINT = "http://127.0.0.1:8080/realms/apps/"
+
+
+oauth_config = OAuth2Config(
+    allow_http=True,
+    jwt_secret=JWT_SECRET,
+    jwt_expires=JWT_EXPIRES,
+    jwt_algorithm=JWT_ALGORITHM,
+    clients=[
+        OAuth2Client(
+            backend=OIDConnect,
+            client_id=OAUTH_CLIENT_ID,
+            client_secret=OAUTH_CLIENT_SECRET,
+        )
+    ]
+)
+
 app = FastAPI()
-
-app.add_middleware(CustomAuthMiddleware, backend=BasicAuthBackend())
-
-
-@app.post("/auth/register")
-async def register_user(user: UserCreate) -> User:
-    return User(**register(user).dict())
+app.add_middleware(OAuth2Middleware, config=oauth_config, callback=on_auth)
+app.include_router(oauth2_router)
 
 
-@app.post("/auth/activate")
-async def activate_user(user: UserActivate):
-    activate_user_controller(user)
+@app.get("/")
+async def index(request: Request,  _: str = Depends(oauth2)):
+    return {"user": request.user}
 
-
-@app.post("/auth/login")
-async def user_login(credentials: Login) -> LoginResponse:
-    return login_user(credentials)
--- web/response/pagination.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class PaginatedResponse(BaseModel):
-    total: int
-    items: list
